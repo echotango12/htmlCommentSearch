efff1274bf6854766736501c64da91ea82617cb3
Ethan <97413997+ethaninfosec@users.noreply.github.com>
2024-08-10 10:35:30 -0500

    Renamed functions and variables to attempt PEP8 compliance

--- get_comments.py
+++ get_comments.py
@@ -5,7 +5,8 @@ import requests
 import sys
 from bs4 import BeautifulSoup, Comment
 
-def getSoup(html):
+
+def get_soup(html):
     try:
         soup = BeautifulSoup(html, features='html.parser')
     except Exception as e:
@@ -14,13 +15,13 @@ def getSoup(html):
     
     return soup
 
-def getSoupFromFile(fileName):
-    print('[*] looking for comments in', fileName)
-    with open(fileName) as fp:
-        soup = getSoup(fp)
+def get_soup_from_file(file_name):
+    print('[*] looking for comments in', file_name)
+    with open(file_name) as fp:
+        soup = get_soup(fp)
     return soup
 
-def getSoupFromSite(url):
+def get_soup_from_site(url):
     print('[*] looking for comments in', url)
     try:
         r = requests.get(url)
@@ -28,25 +29,25 @@ def getSoupFromSite(url):
         print('exception while making request to site:', format(e))
         sys.exit()
 
-    soup = getSoup(r.text)
+    soup = get_soup(r.text)
     return soup
 
-def printComments(soup):
+def print_comments(soup):
     comments = soup.find_all(string=lambda x: isinstance(x, Comment))
-    numComments = len(comments)
+    num_comments = len(comments)
     
-    if numComments == 0:
+    if num_comments == 0:
         print('[-] no comments found')
         return
     
-    print('[+] %s comments found:' % numComments)
+    print('[+] %s comments found:' % num_comments)
     print('-' * 25)
     for comment in comments:
         print(comment)
         print('-' * 25)
     print('[*] all comments printed')
 
-def argParse():
+def arg_parse():
     parser = argparse.ArgumentParser(
                         prog='comment extractor',
                         description='extracts comments from html files and webpages')
@@ -57,16 +58,16 @@ def argParse():
 
 
 def main():
-    args = argParse()
+    args = arg_parse()
     if args.file:
-        soup = getSoupFromFile(args.file)
+        soup = get_soup_from_file(args.file)
     elif args.url:
-        soup = getSoupFromSite(args.url)
+        soup = get_soup_from_site(args.url)
     else:
         print('run "%s --help" for usage' % sys.argv[0])
         sys.exit()
         
-    printComments(soup)
+    print_comments(soup)
 
 if __name__ == '__main__':
     main()
